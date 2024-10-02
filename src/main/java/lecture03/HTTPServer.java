0b026f618fe65943301318ce13f0d3758c428a40
mykhaylo zhovkevych <148889468+mykhaylo-zhovkevych@users.noreply.github.com>
2024-10-02 22:03:32 +0200

    c

--- src/main/java/lecture03/HTTPServer.java
+++ src/main/java/lecture03/HTTPServer.java
@@ -5,10 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-<<<<<<< HEAD
-import java.nio.charset.StandardCharsets;
-=======
->>>>>>> f8d330c3d30c86703150b60fbf90097f0d745154
 
 public class HTTPServer extends AbstractTCPServer{
 
@@ -17,7 +13,6 @@ public class HTTPServer extends AbstractTCPServer{
 		// TODO Auto-generated constructor stub
 	}
 
-<<<<<<< HEAD
 	// prints the message from the client
 	@Override
 	protected void handleConnection(InputStream inputStream, OutputStream outputStream) throws IOException {
@@ -55,19 +50,3 @@ public class HTTPServer extends AbstractTCPServer{
 			}	
 		}
 	}
-=======
-	@Override
-	protected void handleConnection(InputStream inputStream, OutputStream outputStream) throws IOException {
-		// TODO Auto-generated method stub
-		try(InputStreamReader inputStreamReader = new InputStreamReader(inputStream));
-				BufferedReader reader = new BufferedReader(inputStreamReader)) {
-				String line;
-				do {
-					
-				}while(line != null)
-					reader.readLine();	
-				}
-	}
-
-}
->>>>>>> f8d330c3d30c86703150b60fbf90097f0d745154
